624c32b23efa11e14f2281ae0337928421d0b1d3
geoff <geoff.e.newsome@gmail.com>
2020-02-20 21:30:32 -0800

    loop class8

---
+++ class8.js
@@ -0,0 +1,15 @@
+var order = prompt ("enter the shirt size");
+var orderi = parseInt(order) 
+document.write( " you have entere " +orderi + " number of items  ");
+
+var items = [];
+for (var i = 0; i <orderi; i++){
+    var item = prompt ("enter number of shirts");
+    items.push (item)
+
+}
+
+// for (var i = 0; i <items.length; i++){
+//     var = document.write (item)
+// }
+// for additional testing
